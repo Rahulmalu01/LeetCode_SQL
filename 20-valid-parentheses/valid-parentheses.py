30ae8f866622235dab27d728758090ab9f2f7c15
Rahul Malu <rahulmalu25@gmail.com>
2026-06-12 10:45:22 +0530

    Time: 3 ms (74.48%) | Memory: 12.5 MB (69.45%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,17 +1,11 @@
 class Solution(object):
     def isValid(self, s):
-        n = len(s)
-        se = []
+        stack = []
         for i in s:
-            if len(se) != 0:
-                if ord(i) == ord(se[(len(se)) - 1]) + 1 or ord(i) == ord(se[(len(se)) - 1]) + 2:
-                    se.pop()
-                else:
-                    se.append(i)
+            if stack and ord(i) - ord(stack[-1]) in (1, 2):
+                stack.pop()
             else:
-                se.append(i)
-        if not se:
+                stack.append(i)
+        if not stack:
             return True
         return False
-
-        
